fc228c463da6f49686a24c504a7dc52652798fa6
tanishkdhaka <er.tanishkdhaka@gmail.com>
2026-07-23 11:22:18 +0530

    testing somethings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
 
-  distDir: 'dist',
-  images: { unoptimized: true }
-}
-module.exports = nextConfig
+module.exports = nextConfig;
